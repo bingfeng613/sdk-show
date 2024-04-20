c49690f99bea00107b8f9d6281911461e6f7e39c
bingfeng613 <2546414896@qq.com>
2024-04-20 17:09:23 +0800

    针对三种不合规数据进行特殊可视化

--- changeJson.py
+++ changeJson.py
@@ -7,7 +7,7 @@ def parse_data(data):
     items = []
     parts = []
     temp_part = ""
-    depth = 0  
+    depth = 0
 
     for char in data:
         if char == "（":
@@ -36,10 +36,11 @@ def parse_data(data):
             items.append({"main": main_part, "sub": [s.strip() for s in sub_items]})
         else:
             items.append({"main": part.strip(), "sub": []})
-    print(items)
+    # print(items)
     return items
 
-file_path = "beforeTreeData.json"
+
+file_path = "newbeforeTreeData.json"
 with open(file_path, "r", encoding="utf-8") as file:
     original_data = json.load(file)
 
@@ -58,14 +59,91 @@ mind_map = [
 
 next_id = 2  # Resetting next_id for node creation
 
+total_counts = {}  # 统计不合法 数据项数量 sdk-name : numbers
+
 # Populate the mind map with data from each SDK
 for sdk in original_data:
+
+    # 将三个全部拆开
+    more_items = parse_data(sdk["more-data"])
+    fuzzy_items = parse_data(sdk["fuzzy-data"])
+    less_items = parse_data(sdk["less-data"])
+
+    # print("more_items:", more_items)
+    # print("fuzzy_items:",fuzzy_items)
+    # print("less_items:", less_items)
+
+    """
+        处理less_data
+    """
+    count_less_items = 0
+    # 统计数量 后续进行节点关系的绘制
+    if less_items != []:
+        for less_item in less_items:
+            # 括号内子项不为kong
+            if less_item["sub"] != []:
+                count_less_items += len(less_item["sub"]) + 1
+            else:
+                count_less_items += 1
+
+    """
+        处理fuzzy_items
+    """
+    count_fuzzy_items = 0
+    # 统计数量 后续进行节点关系的绘制
+    fuzzy_data_hash = {}
+    # 设置哈希  便于后续进行颜色定位
+    if fuzzy_items != []:
+        for fuzzy_item in fuzzy_items:
+            # 括号内子项不为kong
+            if fuzzy_item["sub"] != []:
+                count_fuzzy_items += len(fuzzy_item["sub"]) + 1
+                for sub_fuzzy_item in fuzzy_item["sub"]:
+                    fuzzy_data_hash[sub_fuzzy_item] = 1 
+            else:
+                count_fuzzy_items += 1
+                fuzzy_data_hash[fuzzy_item["main"]] = 1
+
+    """
+        处理more_items
+    """
+
+    count_more_items = 0
+    # 统计数量 后续进行节点关系的绘制
+    more_data_hash = {}
+    # 设置哈希  便于后续进行颜色定位
+    if more_items != []:
+        for more_item in more_items:
+            # 括号内子项不为kong
+            if more_item["sub"] != []:
+                count_more_items += len(more_item["sub"]) + 1
+                for sub_more_item in more_item["sub"]:
+                    more_data_hash[sub_more_item] = 1
+            else:
+                count_more_items += 1
+                more_data_hash[more_item["main"]] = 1
+
+    print("count_more_items:", count_more_items)
+    print("count_less_items:", count_less_items)
+    print("count_fuzzy_items:", count_fuzzy_items)
+
+    """
+        记录全部的数量 到哈希表中
+    """
+    total_counts[sdk["sdk-name"]] = (
+        count_more_items + count_less_items + count_fuzzy_items
+    )
+
     # start from id=2
     # add sdk-name entry
     sdk_id = str(next_id)
     # 使用 ID 的奇偶性来决定节点的方向
     direction = "verticalRight" if int(sdk_id) % 2 == 0 else "verticalLeft"
 
+    """
+        添加sdk-name项
+        作为次级根节点
+    """
     mind_map.append(
         {
             "id": sdk_id,
@@ -92,6 +170,9 @@ for sdk in original_data:
     )
 
     # add data entry
+    """
+        添加数据项这个根节点
+    """
     sdk_data_id_loc2 = 1
     sdk_data_id = str(next_id) + "." + str(sdk_data_id_loc2)
     mind_map.append(
@@ -104,7 +185,7 @@ for sdk in original_data:
             "stroke": "#00C7B5",
             "headerColor": "#00C7B5",
             "strokeType": "line",
-            "open":False
+            "open": False,
         }
     )
     mind_map.append(
@@ -118,53 +199,80 @@ for sdk in original_data:
             "cornersRadius": 0,
         }
     )
+
+    """
+        添加sdk["data"]中的全部内容
+        根据括号进行层级关系的拆分
+    """
     # add data sub item
     data_items = parse_data(sdk["data"])
     data_sub_start = 1
+
     for index, item in enumerate(data_items):
-        sdk_data_sub_id = sdk_data_id+"."+str(data_sub_start)  # Create a unique sub-node ID
-        # single data item
-        # Append the data node
+        sdk_data_sub_id = (
+            sdk_data_id + "." + str(data_sub_start)
+        )  
+        """
+            节点颜色变换机制
+        """
+        # 进行节点的颜色变换
+        fill_color = "#fff"
+        strokeWidth = 1
+        print("fuzzy_data_hash:",fuzzy_data_hash)
+        print("more_data_hash:",more_data_hash)
+
+        if fuzzy_data_hash.get(item["main"])  == 1:
+            fill_color = "#2349AE"  # 模糊的使用填充蓝色
+            strokeWidth = 6
+
+        if more_data_hash.get(item["main"]) == 1:
+            fill_color = "#D73C2D"  # 多说的使用填充红色
+            strokeWidth = 6
+
         mind_map.append(
             {
                 "id": sdk_data_sub_id,
                 "text": item["main"],
                 "parent": sdk_data_id,
-                "fill": "#fff",
+                "fill": fill_color,
                 "fontColor": "rgba(0,0,0,0.70)",
-                "stroke": "#00C7B5",
                 "headerColor": "#00C7B5",
                 "strokeType": "line",
+                "strokeWidth": strokeWidth,  # 边框线宽
+                "stroke": "#000000",
             }
         )
 
         # Append the connection line
-        mind_map.append({
-            "from": sdk_data_id,
-            "to": sdk_data_sub_id,
-            "type": "line",
-            "connectType": "curved",
-            "stroke": "#CCC",
-            "strokeWidth": 2,
-            "cornersRadius": 0,
-        })
+        mind_map.append(
+            {
+                "from": sdk_data_id,
+                "to": sdk_data_sub_id,
+                "type": "line",
+                "connectType": "curved",
+                "stroke": "#CCC",
+                "strokeWidth": 2,
+                "cornersRadius": 0,
+            }
+        )
 
-        if item['sub']:
+        if item["sub"]:
             # contains ()
             data_sub_sub_start = 1
-            for sub_data in item['sub']:
+            for sub_data in item["sub"]:
                 # single data item
                 # Append the data node
                 mind_map.append(
                     {
-                        "id": sdk_data_sub_id +"."+ str(data_sub_sub_start),
+                        "id": sdk_data_sub_id + "." + str(data_sub_sub_start),
                         "text": sub_data,
                         "parent": sdk_data_sub_id,
-                        "fill": "#fff",
+                        "fill": fill_color,
                         "fontColor": "rgba(0,0,0,0.70)",
-                        "stroke": "#00C7B5",
                         "headerColor": "#00C7B5",
                         "strokeType": "line",
+                        "strokeWidth": strokeWidth,  # 边框线宽
+                        "stroke": "#000000",
                     }
                 )
 
@@ -180,9 +288,81 @@ for sdk in original_data:
                         "cornersRadius": 0,
                     }
                 )
-                data_sub_sub_start+=1
-        data_sub_start+=1
+                data_sub_sub_start += 1
 
+        data_sub_start += 1
+
+    """
+            添加当前sdK-name中的 less-data
+            使用虚线样式
+        """
+
+    for index, item in enumerate(less_items):
+        sdk_data_sub_id = sdk_data_id + "." + str(data_sub_start)
+        mind_map.append(
+            {
+                "id": sdk_data_sub_id,
+                "text": item["main"],
+                "parent": sdk_data_id,
+                "fontColor": "rgba(0,0,0,0.70)",
+                "stroke": "#2349AE",
+                "headerColor": "#00C7B5",
+                "strokeType": "dash",  # 虚线样式
+                "strokeWidth": 6,  # 边框线宽
+            }
+        )
+
+        # Append the connection line
+        mind_map.append(
+            {
+                "from": sdk_data_id,
+                "to": sdk_data_sub_id,
+                "type": "line",
+                "connectType": "curved",
+                "stroke": "#CCC",
+                "strokeWidth": 2,
+                "cornersRadius": 0,
+            }
+        )
+
+        if item["sub"]:
+            # contains ()
+            data_sub_sub_start = 1
+            for sub_data in item["sub"]:
+                # single data item
+                # Append the data node
+                mind_map.append(
+                    {
+                        "id": sdk_data_sub_id + "." + str(data_sub_sub_start),
+                        "text": sub_data,
+                        "parent": sdk_data_sub_id,
+                        "fontColor": "rgba(0,0,0,0.70)",
+                        "stroke": "#2349AE",
+                        "headerColor": "#00C7B5",
+                        "strokeType": "dash",  # 虚线样式
+                        "strokeWidth": 6,  # 边框线宽
+                    }
+                )
+
+                # Append the connection line
+                mind_map.append(
+                    {
+                        "from": sdk_data_sub_id,
+                        "to": sdk_data_sub_id + "." + str(data_sub_sub_start),
+                        "type": "line",
+                        "connectType": "curved",
+                        "stroke": "#CCC",
+                        "strokeWidth": 2,
+                        "cornersRadius": 0,
+                    }
+                )
+                data_sub_sub_start += 1
+
+        data_sub_start += 1
+
+    """
+        添加URL链接节点项
+    """
     # add url link
     sdk_data_id_loc2 += 1
     sdk_url_id = str(next_id) + "." + str(sdk_data_id_loc2)
@@ -213,5 +393,9 @@ for sdk in original_data:
 
     next_id += 1
 
+
+"""
+    写入转换格式后的json文件
+"""
 with open("afterTreeData.json", "w", encoding="utf-8") as file:
     json.dump(mind_map, file, ensure_ascii=False, indent=4)
